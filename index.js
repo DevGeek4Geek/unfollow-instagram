7086a0826ddd4670af3c7b058b373c8bf42649e0
RafaelGSS <rafael.nunu@hotmail.com>
2019-05-12 22:37:01 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+/**
+ * Just tested with resolution width greather than 736px
+ */
+
+const btnFollowing = document.getElementsByClassName('-nal3')[2]
+const btnFollowers = document.getElementsByClassName('-nal3')[1]
+
+const scrollSizeBase = 20
+const unfollowTime = 5000 // Each 5 seconds
+
+function unfollow(idx) {
+    unfollowId(idx)
+    if (idx > 0) {
+        setTimeout(_ => unfollow(idx - 1), 5000)
+    }
+}
+
+function unfollowId(id) {
+    document.getElementsByClassName('PZuss')[0].getElementsByTagName('li')[id].getElementsByTagName('button')[0].click()
+    setTimeout(_ => document.getElementsByClassName('mt3GC')[0].getElementsByTagName('button')[0].click(), 200)
+}
+
+function scrollDivToBottom(idx) {
+    document.getElementsByClassName('isgrP')[0].scrollTop =
+        document.getElementsByClassName('isgrP')[0].scrollHeight
+    if (idx > 0) {
+        setTimeout(_ => scrollDivToBottom(idx - 1), 500)
+    } else {
+        // Call unfollow after scroll to end
+        let followSize = document.getElementsByClassName('PZuss')[0].getElementsByTagName('li').length - 1
+        unfollow(followSize)
+    }
+}
+
+btnFollowing.click();
+// SetTimeout because fade event after click
+setTimeout(_ => scrollDivToBottom(scrollSizeBase), 1000)
